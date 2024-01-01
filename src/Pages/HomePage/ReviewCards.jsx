9650e2782660a029873f98a1c826525b87148858
Kiguradze <bkiguradze@gmail.com>
2024-01-01 19:33:59 +0400

    modify: ReviewCards

--- src/Pages/HomePage/ReviewCards.jsx
+++ src/Pages/HomePage/ReviewCards.jsx
@@ -2,7 +2,7 @@ import ReviewCard from "../../components/ReviewCard";
 
 const ReviewCards = () => {
   return (
-    <section className="w-full mt-[70px] px-4 py-4 flex justify-between gap-[50px] flex-nowrap xl:w-[90%] mx-auto overflow-x-auto overflow-y-hidden">
+    <section className="w-full mt-[70px] px-4 py-4 flex justify-between gap-[50px] flex-nowrap overflow-x-auto overflow-y-hidden xl:w-[90%] xl:overflow-visible mx-auto ">
       <ReviewCard />
       <ReviewCard />
       <ReviewCard />
